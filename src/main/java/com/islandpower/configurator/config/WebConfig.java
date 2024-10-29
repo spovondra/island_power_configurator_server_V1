efa60c8f0bef0cdf09d308725442764bc9787fc9
spovondra <spovondra@gmail.com>
2024-10-29 20:57:47 +0100

    zabazpečeno + přidán certbot pro SSL + povoleny jen localhost a api.fve.firmisimo.eu adresy

--- src/main/java/com/islandpower/configurator/config/WebConfig.java
+++ src/main/java/com/islandpower/configurator/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Configuration class for setting up CORS (Cross-Origin Resource Sharing) in the application.
  * This class configures CORS settings to allow or restrict cross-origin requests.
  *
- * @version 1.0
+ * @version 1.1
  */
 @Configuration
 public class WebConfig {
@@ -24,12 +24,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                // Configure CORS for the /api/** endpoints
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*") // Allow all origins - FOR TESTING PURPOSES ONLY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specified HTTP methods
-                        .allowedHeaders("*"); // Allow all headers
-                        //.allowCredentials(true); // FOR TESTING PURPOSES ONLY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        .allowedOrigins("http://localhost:3000", "https://fve.firmisimo.eu")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
